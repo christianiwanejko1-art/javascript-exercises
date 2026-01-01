a8eaa51308e15b353bf3a368d1bd0e0be9aeb4ec
Christian Iwanejko <christianiwanejko1@gmail.com>
2026-01-01 04:46:06 -0800

    completed joinstrings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,6 +8,15 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos"
+const lastName = "Stevenson"
+const thisYear = 1965
+const birthYear = 1947
+let age = thisYear - birthYear
+let fullName = firstName + " " + lastName
+
+
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age + " years old."
 
 
 
